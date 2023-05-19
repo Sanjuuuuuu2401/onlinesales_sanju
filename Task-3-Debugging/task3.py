d11fa0a6d3c550a794be4a09acc14397d7730238
Sanjay Shettigar <103980079+Sanjuuuuuu2401@users.noreply.github.com>
2023-05-20 00:36:10 +0530

    Add files via upload

---
+++ Task-3-Debugging/task3.py
@@ -0,0 +1,25 @@
+
+
+
+
+
+hd
+
+def compute(n):
+    if n < 10:
+        out = n ** 2
+    elif n < 20:
+        out = 1
+        for i in range(1, n-10+1):
+            #in this case,we have to increase the limit from n-10 to n-10+1;
+            out *= i
+    else:
+        lim = n - 20
+        out = lim * lim
+        out = out - lim
+        out = out / 2 
+    print(out)
+
+
+n = int(input("Enter an integer: "))
+compute(n)
